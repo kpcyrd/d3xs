793966b9c4fb4ee06615ef65a807fa0bc2b2ca39
kpcyrd <git@rxv.cc>
2023-10-23 00:39:43 +0200

    Add project layout for firmware

---
+++ firmware/README.md
@@ -0,0 +1,7 @@
+# d3xs-firmware
+
+Firmware for `esp32-c3` microcontroller (riscv32imc-unknown-none-elf).
+
+```
+RUSTC_BOOTSTRAP=1 cargo build --release
+```
---
+++ firmware/src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_println::println;
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    println!("Hello world!");
+
+    // Set GPIO7 as an output, and set its state high initially.
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    loop {}
+}
